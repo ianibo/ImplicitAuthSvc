63784c4328c038d259f121ace98ffda751a36332
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-05-31 15:21:22 +0100

    Added domain classes to represent users and filters ready for preauth processing

---
+++ ImplicitAuthService/grails-app/domain/implicitauth/Role.groovy
@@ -0,0 +1,24 @@
+package implicitauth
+
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+class Role implements Serializable {
+
+	private static final long serialVersionUID = 1
+
+	String authority
+
+	Role(String authority) {
+		this()
+		this.authority = authority
+	}
+
+	static constraints = {
+		authority blank: false, unique: true
+	}
+
+	static mapping = {
+		cache true
+	}
+}
