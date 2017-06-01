f2403fa690ecca5c69d7a0bc3d231f0e8bf3c1bc
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-06-01 11:55:24 +0100

    updated

--- ImplicitAuthService/grails-app/controllers/implicitauth/PingController.groovy
+++ ImplicitAuthService/grails-app/controllers/implicitauth/PingController.groovy
@@ -1,8 +1,12 @@
 package implicitauth
 
+import grails.converters.JSON;
+
 class PingController {
 
   def index() { 
+    def result = [ 'response' : 'ok' ]
     log.debug("PingController::index ${params} ${request.getHeader('Authorization')}");
+    render result as JSON
   }
 }
