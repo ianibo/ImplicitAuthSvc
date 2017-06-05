db979007ace3f1d097067359bcf0a389a9061f14
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-06-05 14:05:39 +0100

    Merge changes to ping controller

--- ImplicitAuthService/grails-app/controllers/implicitauth/PingController.groovy
+++ ImplicitAuthService/grails-app/controllers/implicitauth/PingController.groovy
@@ -1,9 +1,12 @@
 package implicitauth
 
 import grails.converters.JSON;
+import grails.plugin.springsecurity.annotation.Secured
+
 
 class PingController {
 
+  @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
   def index() { 
     def result = [ 'response' : 'ok' ]
     log.debug("PingController::index ${params} ${request.getHeader('Authorization')}");
