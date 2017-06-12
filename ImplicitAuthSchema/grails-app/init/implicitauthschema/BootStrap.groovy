7dc693dc45e76888de9da62180cd30ceedd8aa70
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-06-12 11:06:20 +0100

    Copy domain classes from ImplicitAuthSvc into ImplicitAuthSchema plugin

---
+++ ImplicitAuthSchema/grails-app/init/implicitauthschema/BootStrap.groovy
@@ -0,0 +1,9 @@
+package implicitauthschema
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
---
+++ demoSvc/grails-app/controllers/demosvc/PingController.groovy
@@ -0,0 +1,11 @@
+package demosvc
+
+
+import grails.rest.*
+import grails.converters.*
+
+class PingController {
+	static responseFormats = ['json', 'xml']
+	
+    def index() { }
+}
